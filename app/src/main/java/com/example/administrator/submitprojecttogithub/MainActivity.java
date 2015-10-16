97a333742b22ce7d7ba354ce9b0dd8cee8d0baa6
penggit <pengyuantao5354@126.com>
2015-10-16 14:37:09 +0800

    我的第一次提交github

---
+++ app/src/main/java/com/example/administrator/submitprojecttogithub/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.administrator.submitprojecttogithub;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * 提交到github上的示例
+ */
+public class MainActivity extends ActionBarActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
